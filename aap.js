5e70643c5a5c460938a6b55322f4225d8fbc5f15
Abdullah30nov <abdullahbinaslam64@gmail.comgit config --global user.email abdullahbinaslam64@gmail.com>
2024-03-03 03:42:31 -0800

    Assignment-02 Array JS

---
+++ aap.js
@@ -0,0 +1,45 @@
+
+                    //    CHAPTER-NO-14-16
+                    // QUESTION-NO-01
+// var educ = "Qualification"
+// var student1 = "SSC"
+// var student2 = "HSC"
+// var student3 = "BCS"
+// var student4 = "BS"
+// var student5 = "BCOM"
+// var student6 = "MS"
+// var student7 = "M.Phil"
+// var student8 = "Phd"
+
+// var array=["Qualification"+ "<br><br>" +"1) SSC"+ "<br>" +"2) HSC"+ "<br>" +"3) BCS"+ "<br>" +"4) BS"+ "<br>" +"5) BCOM"+ "<br>"+ "6) MS" +"<br>"+"7) M.Phil"+ "<br>" +"8) Phd"]
+// document.write(array);
+
+                            //    QUESTION-NO-02
+// var std1 = "Abdullah",std2= "Bilal",std3= "Dawood";
+// var arr1=["Abdullah","Bilal","Dawood"];
+// var scr1 = "320",scr2= "230",scr3= "480";
+// var arr2=["320","230","480"]
+
+// document.write("Score of " + arr1[0] + " is " + arr2[0] + ". Percentage: 64%" +"<br>"+ "Score of " + arr1[1] + " is " + arr2[1] + ". Percentage: 46%" + "<br>"+"Score of " + arr1[2] + " is " + arr2[2] + ". Percentage: 96%"   )
+
+                                                   // QUESTION-NO-03
+// var sos =[320,230,480,120]
+// document.write("Scores of Students: "+ sos + "<br>" + "Ordered Scores of Students: " + sos[3] + "," + sos[1] + "," + sos[0] + "," + sos[2])
+
+                                                   // QUESTION-NO-04
+// var array=["Karachi","Lahore","Islamabad","Quetta","Peshawar"];
+// document.write("<h1>Cities List: </h1><br>" + array + "<br><h1>Selected cities list: </h1>" + array[2] + "," + array[3])
+
+                                                   // QUESTION-NO-05
+// var arr = ["This ","is ","my ","cat"];
+// document.write("<h2>ARRAY: </h2><br>" + arr + "<br><h2>String: </h2>" + arr[0]+arr[1]+arr[2]+arr[3])
+                                                   // QUESTION-NO-06
+// var arr = ["Keyboard","mouse","printer","monitor"];
+// var out=["Keyboard","mouse","printer","monitor","Out:"];
+// document.write("<h2>Devices: </h2><br>" + arr + "<br><br>");
+// document.write( out[4],"<br>",out[0],"<br>",out[4],"<br>",out[1],"<br>",out[4],"<br>",out[2],"<br>",out[4],"<br>",out[3],"<br>")
+// document.write("<h2>Devices</h2>" ,"<br>",out[4],"<br>",out[3],"<br>",out[4],"<br>",out[2],"<br>",out[4],"<br>",out[1],"<br>",out[4],"<br>",out[0])
+
+
+
+
